3028d7f4bd095bccf8a2661236f2b5a415205e70
Robert Wagner <rw@20sec.net>
2026-04-10 22:03:05 +0200

    remove pre tags (from table)

--- src/terminal.php
+++ src/terminal.php
@@ -38,6 +38,8 @@ class terminal {
             '</blink>' => color::reset_blink(),
             '<inv>' => color::inverse(),
             '</inv>' => color::reset_inverse(),
+            '<pre>' => '',
+            '</pre>' => '',
             '<ok>' => color::bold() . color::green->bg() . color::white->fg() . ' ',
             '</ok>' => ' ' . color::reset()
         ];
